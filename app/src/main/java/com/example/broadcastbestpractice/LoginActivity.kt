c986e02896c9156ba498dee298e7a4fd207589b2
xiao <gds1105928109@qq.com>
2020-11-16 10:57:16 +0800

    修改了App图标,实现了登录功能

--- app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,13 +1,26 @@
 package com.example.broadcastbestpractice
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-//       supportActionBar?.hide()//标题弃掉
+        LoginButton.setOnClickListener {
+            val id:CharSequence=UserID.text.trim().toString()
+            val pwd:String=UserPwd.text.trim().toString()
+            if (id=="1105928109"&&pwd=="123456"){
+                val intent=Intent(this,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else{
+                Toast.makeText(this,"QQ号码或密码错误,请重试",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
