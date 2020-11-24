a3aca544a978f76cebdf4c900e075fe61c3d88a3
xiao <gds1105928109@qq.com>
2020-11-24 09:36:25 +0800

    已完成记住密码功能

--- app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,20 +1,41 @@
 package com.example.broadcastbestpractice
 
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val prefs =getPreferences(Context.MODE_PRIVATE)
+        val isRemember =prefs.getBoolean("remember_password",false)
+        if (isRemember){
+            val account =prefs.getString("User","")
+            val pwd =prefs.getString("Pwd","")
+            UserID.setText(account)
+            UserPwd.setText(pwd)
+            checkBoxpass.isChecked =true
+        }
+
         LoginButton.setOnClickListener {
             val id:CharSequence=UserID.text.trim().toString()
             val pwd:String=UserPwd.text.trim().toString()
+
             if (id=="1105928109"&&pwd=="123456"){
+                val editor =prefs.edit()
+                if (checkBoxpass.isChecked){
+                    editor.putBoolean("remember_password",true)
+                    editor.putString("User",id.toString())
+                    editor.putString("Pwd",pwd)
+                }else{
+                    editor.apply()
+                }
+                editor.apply()
+
                 val intent=Intent(this,MainActivity::class.java)
                 startActivity(intent)
                 finish()
@@ -22,5 +43,6 @@ class LoginActivity : AppCompatActivity() {
                 Toast.makeText(this,"QQ号码或密码错误,请重试",Toast.LENGTH_LONG).show()
             }
         }
+
     }
 }
